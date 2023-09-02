a16bb7e4cfb5f842164cc0461441b79f09aa1833
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-09-02 00:26:05 -0400

    google authenication setup

--- models/receipt.js
+++ models/receipt.js
@@ -11,6 +11,11 @@ const ReceiptSchema = new Schema({
 				type: Number,
 				required: true,
 			},
+			quantity: {
+				type: Number,
+				required: true,
+				default: 1,
+			},
 			owner: {
 				type: String,
 			},
