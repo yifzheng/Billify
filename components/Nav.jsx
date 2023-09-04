335c13dda56b7633f1e62eda713a66fc822f9ba2
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-09-03 20:07:53 -0400

    fixed bug with mongodb where image was not being created in the schema

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -23,7 +23,7 @@ const Nav = () => {
         }
         setUpProviders()
     }, [] )
-    console.log( providers )
+    
     return (
         <nav className='flex-between w-full mb-16 pt-3'>
             <Link href={ "/" } className='flex gap-2 flex-center' >
