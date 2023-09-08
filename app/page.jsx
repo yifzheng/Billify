23c0f3d91df1025ea0a212ac252df370082a337d
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-09-07 22:48:29 -0400

    modify descriptions on app

--- app/page.jsx
+++ app/page.jsx
@@ -11,8 +11,7 @@ const Home = () => {
     <section className="flex-center flex-col text-center mt-[10%]">
       <h1 className="head_text blue_gradient">Welcome to Billify</h1>
       <span className="desc w-full">
-        Create receipts and find each of your friends contributions
-        between meals and parties.
+        Generate receipts and identify the contributions of each of your acquaintances during gatherings and dining occasions.
       </span>
       { !session && <span className="desc w-full"><b>Sign in</b> to continue</span> }
       <br />
